7dfd862d617373708262424278232730c8f5a4de
Yogesh <akshayjr69@gmail.com>
2025-09-03 21:34:23 +0530

    added rds and multi env support

---
+++ knexfile.js
@@ -0,0 +1,49 @@
+// Update with your config settings.
+
+require('dotenv').config();
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    },
+    migrations: {
+      directory: './migrations',
+    }
+  },
+  test: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    },
+    migrations: {
+      directory: './migrations',
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    },
+    migrations: {
+      directory: './migrations',
+    },
+    pool: {
+      min: 2,
+      max: 10
+    }
+  }
+};
