659b2d79a1424c0d6ff529a266097f7e05784ba8
Yogesh <akshayjr69@gmail.com>
2025-09-03 23:13:04 +0530

    refactor(knexfile): consolidate database configuration into masterConfig for cleaner code

--- knexfile.js
+++ knexfile.js
@@ -2,23 +2,25 @@
 
 require('dotenv').config();
 
+const masterConfig = {
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: process.env.DB_HOST ? { rejectUnauthorized: false } : false,
+  },
+  migrations: {
+    directory: './migrations',
+  },
+};
+
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-  development: {
-    client: 'pg',
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      ssl: {
-        rejectUnauthorized: false
-      }
-    },
-    migrations: {
-      directory: './migrations',
-    }
-  }
+  development: { ...masterConfig },
+  testing: { ...masterConfig },
+  production: { ...masterConfig },
 };
